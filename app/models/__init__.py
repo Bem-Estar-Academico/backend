5a79434d00c61f3502529e19e3a377219d3ddf7b
Ruan Melo <ruantmelo@gmail.com>
2025-10-30 16:12:18 -0300

    Update models config to include entity Appeal

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.notice import Document, Notice, NoticeTeam
 from app.models.registration import StudentRegistration
 from app.models.review import ReviewRegistrationModel
 from app.models.user import User
+from app.models.appeal import Appeal
 
-__all__ = ["User", "Notice", "Document", "NoticeTeam", "StudentRegistration", "ReviewRegistrationModel"]
+__all__ = ["User", "Notice", "Document", "NoticeTeam", "StudentRegistration", "ReviewRegistrationModel", "Appeal"]
