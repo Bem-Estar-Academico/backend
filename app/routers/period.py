13f5943839b09117a74890fcea68dc1501c49bce
mestrevh <victor-torugo@hotmail.com>
2025-11-08 22:57:54 -0300

    feat: added the router get all period

--- app/routers/period.py
+++ app/routers/period.py
@@ -1,11 +1,13 @@
+from typing import List
 from fastapi import APIRouter, Depends, status 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.database import get_db
+from app.models.period import Period
 from app.schemas.period import PeriodCreate, PeriodResponse
 from app.services.period_service import PeriodService 
 
-router = APIRouter()
+router = APIRouter(tags=["periods"])
 
 @router.post(
     "/",
@@ -21,3 +23,18 @@ async def create_period(
     period_res = await PeriodService.create_period(db=db, period=period)
     
     return period_res
+
+@router.get(
+    "/",
+    response_model=List[PeriodResponse],
+    summary="Lista todos os períodos"
+)
+async def get_all_periods(
+    db: AsyncSession = Depends(get_db)
+) -> List[Period]: 
+    """
+    router the get all periods
+    """
+    periods = await PeriodService.get_all_periods(db=db)
+    
+    return periods
--- app/services/period_service.py
+++ app/services/period_service.py
@@ -1,10 +1,13 @@
+from typing import List
+from sqlalchemy import select
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.period import Period
 from app.schemas.period import PeriodCreate
 
 class PeriodService:
     
-    async def create_period(db: Session, period: PeriodCreate) -> Period:
+    async def create_period(db: AsyncSession, period: PeriodCreate) -> Period:
         period_data = period.model_dump()
         
         db_period = Period(**period_data)
@@ -14,3 +17,14 @@ class PeriodService:
         await db.refresh(db_period)
         
         return db_period
+    
+    async def get_all_periods(db: AsyncSession) -> List[Period]:
+        """
+        get all period in the database.
+        """
+        
+        query = select(Period).order_by(Period.init_date)
+
+        result = await db.execute(query)
+
+        return list(result.scalars().all())
